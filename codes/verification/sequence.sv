9296acb83a29cf2f114843f0c23431446eae2708
G Yashwanth Kumar <102477190+yashyash0110@users.noreply.github.com>
2025-04-04 19:29:43 +0530

    Update sequence.sv

--- codes/verification/sequence.sv
+++ codes/verification/sequence.sv
@@ -1,10 +1,10 @@
-//Sequence Class
-class seq extends uvm_sequence#(packet);
-  `uvm_object_utils(seq)
+//Sequences Class
+class write_data extends uvm_sequence#(packet);
+  `uvm_object_utils(write_data)
   
   packet pkt;
   
-  function new(input string name = "seq");
+  function new(input string name = "write_data");
     super.new(name);
   endfunction
   
@@ -12,11 +12,188 @@ class seq extends uvm_sequence#(packet);
     repeat(15)
       begin
         pkt = packet::type_id::create("pkt");
+        pkt.addr_c.constraint_mode(1);
+        pkt.addr_c_err.constraint_mode(0);
+
         start_item(pkt);
         pkt.randomize();
-        `uvm_info(get_type_name(),$sformatf("a: %d b:%d y:%d",pkt.a,pkt.b,pkt.y),UVM_MEDIUM);
+        pkt.op = WRITE;
         finish_item(pkt);
       end
   endtask
   
+endclass
+
+///////////////////////////////////////////////////////////
+
+class read_data extends uvm_sequence#(packet);
+  `uvm_object_utils(read_data)
+  
+  packet pkt;
+  
+  function new(input string name = "read_data");
+    super.new(name);
+  endfunction
+  
+  virtual task body();
+    repeat(15)
+      begin
+        pkt = packet::type_id::create("pkt");
+        pkt.addr_c.constraint_mode(1);
+        pkt.addr_c_err.constraint_mode(0);
+        
+        start_item(pkt);
+        pkt.randomize();
+        pkt.op = READ;
+        finish_item(pkt);
+      end
+  endtask
+  
+endclass
+
+///////////////////////////////////////////////////////////
+
+class write_read extends uvm_sequence#(packet);
+  `uvm_object_utils(write_read)
+  
+  packet pkt;
+  
+  function new(input string name = "write_read");
+    super.new(name);
+  endfunction
+  
+  virtual task body();
+    repeat(15)
+      begin
+        pkt = packet::type_id::create("pkt");
+        pkt.addr_c.constraint_mode(1);
+        pkt.addr_c_err.constraint_mode(0);
+        
+        start_item(pkt);
+        pkt.randomize();
+        pkt.op = WRITE;
+        finish_item(pkt);
+        
+        start_item(pkt);
+        pkt.randomize();
+        pkt.op = READ;
+        finish_item(pkt);
+      end
+  endtask
+  
+endclass
+
+///////////////////////////////////////////////////////////
+
+class writeb_readdb extends uvm_sequence#(packet);
+  `uvm_object_utils(writeb_readdb)
+  
+  packet pkt;
+  
+  function new(input string name = "writeb_readdb");
+    super.new(name);
+  endfunction
+  
+  virtual task body();
+    repeat(15)
+      begin
+        pkt = packet::type_id::create("pkt");
+        pkt.addr_c.constraint_mode(1);
+        pkt.addr_c_err.constraint_mode(0);
+        
+        start_item(pkt);
+        pkt.randomize();
+        pkt.op = WRITE;
+        finish_item(pkt);
+      end
+    repeat(15)
+      begin
+        pkt = packet::type_id::create("pkt");
+        pkt.addr_c.constraint_mode(1);
+        pkt.addr_c_err.constraint_mode(0);
+        
+        start_item(pkt);
+        pkt.randomize();
+        pkt.op = READ;
+        finish_item(pkt);
+      end
+  endtask
+endclass
+
+///////////////////////////////////////////////////////////
+
+class write_err extends uvm_sequence#(packet);
+  `uvm_object_utils(write_err)
+  
+  packet pkt;
+  
+  function new(input string name = "write_err");
+    super.new(name);
+  endfunction
+  
+  virtual task body();
+    repeat(15)
+      begin
+        pkt = packet::type_id::create("pkt");
+        pkt.addr_c.constraint_mode(1);
+        pkt.addr_c_err.constraint_mode(0);
+        
+        start_item(pkt);
+        pkt.randomize();
+        pkt.op = WRITE;
+        finish_item(pkt);
+      end
+  endtask
+endclass
+
+///////////////////////////////////////////////////////////
+
+class read_err extends uvm_sequence#(packet);
+  `uvm_object_utils(read_err)
+  
+  packet pkt;
+  
+  function new(input string name = "read_err");
+    super.new(name);
+  endfunction
+  
+  virtual task body();
+    repeat(15)
+      begin
+        pkt = packet::type_id::create("pkt");
+        pkt.addr_c.constraint_mode(1);
+        pkt.addr_c_err.constraint_mode(0);
+        
+        start_item(pkt);
+        pkt.randomize();
+        pkt.op = READ;
+        finish_item(pkt);
+      end
+  endtask
+endclass
+
+///////////////////////////////////////////////////////////
+
+class reset_dut extends uvm_sequence#(packet);
+  `uvm_object_utils(reset_dut)
+  
+  packet pkt;
+  
+  function new(input string name = "reset_dut");
+    super.new(name);
+  endfunction
+  
+  virtual task body();
+    repeat(15)
+      begin
+        pkt = packet::type_id::create("pkt");
+        pkt.addr_c.constraint_mode(1);
+        pkt.addr_c_err.constraint_mode(0);
+        
+        start_item(pkt);
+        pkt.randomize();
+        pkt.op = RESET;
+        finish_item(pkt);
+      end
+  endtask
 endclass
