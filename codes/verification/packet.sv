5f6c4c3331f7291c49479d3112dac0df2a95f443
G Yashwanth Kumar <102477190+yashyash0110@users.noreply.github.com>
2025-04-04 19:35:02 +0530

    Update packet.sv

--- codes/verification/packet.sv
+++ codes/verification/packet.sv
@@ -1,10 +1,29 @@
+typedef enum bit [1:0] {READ=0,WRITE=1,RESET=2} oper_mode;
+
 //Transaction or Packet or sequence item Class
 class packet extends uvm_sequence_item;
-  `uvm_object_utils(packet)
   
-  rand bit [3:0] a;
-  rand bit [3:0] b;
-  bit [7:0] y;
+  rand oper_mode op;
+  rand logic  PWRITE;
+  rand logic [31:0] PWDATA;
+  rand logic [31:0] PADDR;
+  
+  //Output Signals of DUT for APB UART's transaction
+  logic PREADY;
+  logic PSLVERR;
+  logic [31:0] PRDATA;
+  
+  `uvm_object_utils_begin(packet)
+  `uvm_field_int(PWRITE,UVM_ALL_ON)
+  `uvm_field_int(PWDATA,UVM_ALL_ON)
+  `uvm_field_int(PADDR,UVM_ALL_ON)
+  `uvm_field_int(PREADY,UVM_ALL_ON)
+  `uvm_field_int(PSLVERR,UVM_ALL_ON)
+  `uvm_field_int(PRDATA,UVM_ALL_ON)
+  `uvm_object_utils_end
+  
+  constraint addr_c {PADDR <= 31; }
+  constraint addr_c_err {PADDR > 31; }
   
   function new(input string name = "packet");
     super.new(name);
