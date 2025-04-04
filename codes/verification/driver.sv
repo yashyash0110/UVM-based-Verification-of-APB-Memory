c38a5b8cade4ae22b710f907d8a9e3af4ac06963
G Yashwanth Kumar <102477190+yashyash0110@users.noreply.github.com>
2025-04-04 19:32:26 +0530

    Update driver.sv

--- codes/verification/driver.sv
+++ codes/verification/driver.sv
@@ -1,8 +1,9 @@
 //Driver Class
 class driver extends uvm_driver#(packet);
   `uvm_component_utils(driver)
+  
   packet pkt;
-  virtual mul_intf mif;
+  virtual apb_intf apbif;
   
   function new(string name = "driver",uvm_component parent);
     super.new(name,parent);
@@ -10,7 +11,7 @@ class driver extends uvm_driver#(packet);
   
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if(!uvm_config_db#(virtual mul_intf)::get(this,"","mif",mif))
+    if(!uvm_config_db#(virtual apb_intf)::get(this,"","apbif",apbif))
       `uvm_error(get_type_name(),"Unable to access Interface")
     else
       `uvm_info(get_type_name(),"Successfully got access to Interface",UVM_MEDIUM)
@@ -19,15 +20,64 @@ class driver extends uvm_driver#(packet);
     
   endfunction
   
-  virtual task run_phase(uvm_phase phase);
+  task reset_dut();
+    repeat(5)
+      begin
+        apbif.PRESET <= 1'b0;
+        apbif.PADDR <= 'h0;
+        apbif.PWDATA <= 'h0;
+        apbif.PWRITE <= 'b0;
+        apbif.PSEL <= 'b0;
+        apbif.PENABLE <= 'b0;
+        `uvm_info(get_type_name(),"System Reset : Start of Simulation",UVM_MEDIUM)@(posedge apbif.PCLK);
+      end
+  endtask
+  
+  task drive();
+    reset_dut();
     forever begin
       seq_item_port.get_next_item(pkt);
-      mif.a <= pkt.a;
-      mif.b <= pkt.b;
-      `uvm_info(get_type_name(),$sformatf("a:%d b:%d y:%d",pkt.a,pkt.b,pkt.y),UVM_MEDIUM);
+      
+      if (pkt.op == RESET)
+        begin
+          apbif.PRESET <= 1'b0;
+          @(posedge apbif.PCLK);
+        end
+      
+      else if (pkt.op == WRITE)
+        begin
+          apbif.PSEL <= 1'b1;
+          apbif.PADDR <= pkt.PADDR;
+          apbif.PWDATA <= pkt.PWDATA;
+          apbif.PRESET <= 1'b1;
+          apbif.PWRITE <= 1'b1;
+          @(posedge apbif.PCLK);
+          apbif.PENABLE <= 1'b1;
+          `uvm_info(get_type_name(),$sformatf("Mode:%0s, addr:%0d, wdata:%0d, rdata:%0d, slverr:%0d",pkt.op.name(),pkt.PADDR,pkt.PWDATA,pkt.PRDATA,pkt.PSLVERR),UVM_MEDIUM)
+          @(negedge apbif.PREADY);
+          apbif.PENABLE <= 1'b0;
+          pkt.PSLVERR <= apbif.PSLVERR;
+        end
+      else if (pkt.op == READ)
+        begin
+          apbif.PSEL <= 1'b1;
+          apbif.PADDR <= pkt.PADDR;
+          apbif.PRESET <= 1'b1;
+          apbif.PWRITE <= 1'b1;
+          @(posedge apbif.PCLK);
+          apbif.PENABLE <= 1'b1;
+          `uvm_info(get_type_name(),$sformatf("Mode:%0s, addr:%0d, wdata:%0d, rdata:%0d, slverr:%0d",pkt.op.name(),pkt.PADDR,pkt.PWDATA,pkt.PRDATA,pkt.PSLVERR),UVM_MEDIUM)
+          @(negedge apbif.PREADY);
+          apbif.PENABLE <= 1'b0;
+          pkt.PRDATA = apbif.PRDATA;
+          pkt.PSLVERR = apbif.PSLVERR;
+        end
       seq_item_port.item_done();
-      #20;
     end
+  endtask
+  
+  virtual task run_phase(uvm_phase phase);
+    drive();
   endtask
   
 endclass
