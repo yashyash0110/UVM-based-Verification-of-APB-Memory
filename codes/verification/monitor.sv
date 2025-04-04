b13543c17d27fcaca0ab5b82a775093576811242
G Yashwanth Kumar <102477190+yashyash0110@users.noreply.github.com>
2025-04-04 19:34:15 +0530

    Update monitor.sv

--- codes/verification/monitor.sv
+++ codes/verification/monitor.sv
@@ -3,7 +3,7 @@ class monitor extends uvm_monitor;
   `uvm_component_utils(monitor)
   
   packet pkt;
-  virtual mul_intf mif;
+  virtual apb_intf apbif;
   
   uvm_analysis_port #(packet) mon_port;
   
@@ -13,7 +13,7 @@ class monitor extends uvm_monitor;
   
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if(!uvm_config_db#(virtual mul_intf)::get(this,"","mif",mif))
+    if(!uvm_config_db#(virtual apb_intf)::get(this,"","apbif",apbif))
       `uvm_error(get_type_name(),"Unable to access Interface")
     else
       `uvm_info(get_type_name(),"Successfully got access to Interface",UVM_MEDIUM)
@@ -24,14 +24,36 @@ class monitor extends uvm_monitor;
   endfunction
     
   virtual task run_phase(uvm_phase phase);
-    forever begin
-      #20;
-      pkt.a = mif.a;
-      pkt.b = mif.b;
-      pkt.y = mif.y;
-      `uvm_info(get_type_name(),$sformatf("a:%d b:%d y:%d",pkt.a,pkt.b,pkt.y),UVM_MEDIUM);
-      mon_port.write(pkt);
-    end
+    forever
+      begin
+        #20;
+        if(!apbif.PRESET)
+          begin
+            pkt.op = RESET;
+            `uvm_info(get_type_name(),"System Reset Detected",UVM_MEDIUM)
+            mon_port.write(pkt);
+          end
+        else if (apbif.PRESET && apbif.PWRITE)
+          begin
+            @(negedge apbif.PREADY);
+            pkt.op = WRITE;
+            pkt.PWDATA = apbif.PWDATA;
+            pkt.PADDR = apbif.PADDR;
+            pkt.PSLVERR = apbif.PSLVERR;
+            `uvm_info(get_type_name(),$sformatf("DATA WRITE addr:%0d data:%0d slverr:%0d",pkt.PADDR,pkt.PWDATA,pkt.PSLVERR),UVM_MEDIUM)
+            mon_port.write(pkt);
+          end
+        else if(apbif.PRESET && !apbif.PWRITE)
+          begin
+            @(negedge apbif.PREADY);
+            pkt.op = READ;
+            pkt.PRDATA = apbif.PRDATA;
+            pkt.PADDR = apbif.PADDR;
+            pkt.PSLVERR = apbif.PSLVERR;
+            `uvm_info(get_type_name(),$sformatf("DATA READ addr:%0d data:%0d slverr:%0d",pkt.PADDR,pkt.PWDATA,pkt.PSLVERR),UVM_MEDIUM)
+            mon_port.write(pkt);
+          end
+      end
   endtask
   
 endclass
