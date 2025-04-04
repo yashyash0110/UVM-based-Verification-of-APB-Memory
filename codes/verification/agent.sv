bded0868a70cc8d3fedb373fa563e17698866976
G Yashwanth Kumar <102477190+yashyash0110@users.noreply.github.com>
2025-04-04 19:32:06 +0530

    Update agent.sv

--- codes/verification/agent.sv
+++ codes/verification/agent.sv
@@ -1,3 +1,13 @@
+class config_apb extends uvm_object;
+  `uvm_object_utils(config_apb)
+
+  uvm_active_passive_enum agent_type = UVM_ACTIVE;
+
+  function new(string name = "config_apb");
+    super.new(name);
+  endfunction
+endclass
+
 //Agent Class
 class agent extends uvm_agent;
   `uvm_component_utils(agent)
@@ -6,15 +16,29 @@ class agent extends uvm_agent;
   driver drv;
   monitor mon;
   
+  config_apb cfg;
+  
   function new(string name = "agent",uvm_component parent);
     super.new(name,parent);
   endfunction
   
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    seqr = sequencer::type_id::create("seqr",this);
-    drv = driver::type_id::create("drv",this);
+    
     mon = monitor::type_id::create("mon",this);  
+    
+    cfg = config_apb::type_id::create("cfg",this);
+    seqr = sequencer::type_id::create("seqr",this);
+    
+    if(!uvm_config_db#(config_apb)::get(this,"","cfg",cfg))
+      `uvm_error(get_type_name(),"Failed to access config")
+    
+    if(cfg.agent_type == UVM_ACTIVE)
+      begin
+        drv = driver::type_id::create("drv",this);
+        `uvm_info(get_type_name(),"Successfully got access to config",UVM_MEDIUM)
+      end
+    
   endfunction
   
   virtual function void connect_phase(uvm_phase phase);
